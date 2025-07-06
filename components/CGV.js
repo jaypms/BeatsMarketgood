9c3854120980a123e12681bad10642f2ec89ca8d
jaypms <beatsmarket.best@gmail.com>
2025-07-06 19:53:29 +0200

    Create CGV.js

---
+++ components/CGV.js
@@ -0,0 +1,15 @@
+// components/CGV.js
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+const cgvFR = `
+# Conditions Générales de Vente
+
+Bienvenue sur BeatsMarket.fr
+
+---
+
+## 1. Acceptation des Conditions
+
+L’utilisation de la plateforme BeatsMarket implique l’acceptation sans réserve des présentes Conditions Générales de Vente (CGV).  
+Avant toute inscription,
